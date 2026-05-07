796a2e6cc7eb42a118e0b0cd20f3b89e22cca917
santisierra <santisierra2493@gmail.com>
2026-05-07 09:54:55 -0300

    mostrar coordenadas y ataque enemigo

--- Enemy.gd
+++ Enemy.gd
@@ -2,19 +2,22 @@ extends Node3D
 
 var coordX:=0
 var coordY:=0
+@export var label: Label3D
 
 func _ready() -> void:
 	coordX = randi_range(-10, 10)
 	coordY = randi_range(-10, 10)
 	position = Vector3(coordX,0,coordY)
-	print(coordX," - ",coordY)
-
-func _on_timer_timeout():
-	#enemigo ataca
-	print("Repeating action triggered!")
+	if label:
+		label.text = "("+str(coordX) + " , "+str(coordY)+ ")"
 	
 func check_hit(tx, ty) -> bool:
 	if tx == coordX and ty == coordY:
 		queue_free()
 		return true
 	return false
+
+#tomado desde Signals
+func _on_attack_timer_timeout() -> void:
+	pass # Replace with function body.
+	GameState.take_damage(10) 
