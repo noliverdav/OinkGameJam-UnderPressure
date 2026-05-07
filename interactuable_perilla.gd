d8d94677237a8b6037b00e34a3ed29111521a16f
santisierra <santisierra2493@gmail.com>
2026-05-07 09:54:12 -0300

    limitar grados perilla

--- interactuable_perilla.gd
+++ interactuable_perilla.gd
@@ -8,17 +8,21 @@ var current_degrees: int = 0
 var is_interacting: bool = false
 var _internal_degrees: float = 0.0
 
+func _ready() -> void:
+	update_visual()
 
 func _process(delta):
 	if not is_interacting:
 		return
 	if Input.is_action_pressed("Increse_grades"):
-		_internal_degrees += step * delta * 10
+		if _internal_degrees < 10:
+			_internal_degrees += step * delta * 10
 	if Input.is_action_pressed("Decrese_grades"):
-		_internal_degrees -= step * delta * 10
+		if _internal_degrees > -10:
+			_internal_degrees -= step * delta * 10
+	
 	current_degrees = round(_internal_degrees)
-
-	#current_degrees = clamp(current_degrees, min_degrees, max_degrees)
+	
 	update_visual()
 
 func get_prompt():
