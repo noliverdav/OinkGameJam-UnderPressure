b92b20b097dea9bb55dd79ba00bbd38dfa30980b
santisierra <santisierra2493@gmail.com>
2026-05-07 09:55:34 -0300

    vida player y recibir dano

--- TutorialYoutube/GameState.gd
+++ TutorialYoutube/GameState.gd
@@ -17,7 +17,15 @@ func get_value(key, default_value = null):
 	
 	return default_value
 
-
+func take_damage(amount: int):
+	var current_health = get_value("health", 100)
+	var new_health = current_health - amount
+	
+	set_value("health", max(0, new_health))
+	print(current_health)
+	
+	if new_health <= 0:
+		print("Player Died!")
 
 func set_value(key, value):
 	state[key] = value
