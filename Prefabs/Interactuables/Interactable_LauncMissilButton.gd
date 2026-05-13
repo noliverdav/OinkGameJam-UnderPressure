12d5e9f591bd02d8603b10b735db2f4b51fad241
noliverdav <164519266+noliverdav@users.noreply.github.com>
2026-05-13 18:49:47 -0300

    Small changes made

--- Prefabs/Interactuables/Interactable_LauncMissilButton.gd
+++ Prefabs/Interactuables/Interactable_LauncMissilButton.gd
@@ -1,21 +1,20 @@
 extends Interactable
+
 @export var cannon: Node
 @export var door: Escotilla
-
-
-
-#func _process(delta: float) -> void:
-	#if Coor_x_Node:
-	#	print(Coor_x_Node.current_degrees);
+@export var click_sound: AudioStreamPlayer3D
 
 func interact(body):
 	if not enabled:
 		return
+
+	if click_sound:
+		click_sound.play()
 		
 	if door and door.is_open:
 		print("Puerta abierta")
 		return
 	
 	if cannon and cannon.has_method("fire"):
-		cannon.fire()
-	
+		cannon.fire(body) 
+		print("disparo")
--- Prefabs/Interactuables/interactuable_missile_cannon.gd
+++ Prefabs/Interactuables/interactuable_missile_cannon.gd
@@ -1,7 +1,7 @@
 extends Interactable
 
-var loadState := false;
-signal fireSignal(coordx_value,coordy_value,coordz_value)
+var loadState := false
+signal fireSignal(coordx_value, coordy_value, coordz_value)
 
 @export var missile_mesh: Node3D
 @export var Coor_x_Node: InteractuablePerilla
@@ -9,25 +9,29 @@ signal fireSignal(coordx_value,coordy_value,coordz_value)
 @export var Coor_Z_Node: InteractuablePerilla
 
 func interact(body):
-	
-	if not enabled or GameState.get_value("gabbedMissil")==false:
+	if not enabled or GameState.get_value("gabbedMissil") == false:
 		return
+	
 	GameState.set_value("gabbedMissil", false)
 	loadState = !loadState
 	update_visual()
-
 
 func update_visual():
 	if missile_mesh:
 		missile_mesh.visible = loadState
 		
-func fire():
+func fire(body):
 	if not loadState:
+		print("Cañon vacio camara no se mueve brr")
 		return
-	#if Coor_x_Node and Coor_Y_Node:
-		#print(Coor_x_Node.current_degrees);
-	#if door and door.is_open:
-		#return
+	
 	loadState = false
-	fireSignal.emit(Coor_x_Node.current_degrees,Coor_Y_Node.current_degrees,Coor_Z_Node.current_degrees)
+	
+	var camera_node = body.find_child("Camera3D", true, false)
+	if camera_node:
+		var shaker = camera_node.get_node_or_null("CameraShaker") 
+		if camera_node.has_method("shake"):
+			camera_node.shake(0.8)
+			
+	fireSignal.emit(Coor_x_Node.current_degrees, Coor_Y_Node.current_degrees, Coor_Z_Node.current_degrees)
 	update_visual()
